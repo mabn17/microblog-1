0c472be47c1bff5c06863e6bff9cc60d58725824
Martin Borg <martin.d@live.se>
2019-11-07 22:57:29 +0100

    edited deploy-scripts

--- scripts/deploy-app/4.supervisor.sh
+++ scripts/deploy-app/4.supervisor.sh
@@ -1,4 +1,4 @@
-sudo cp scripts/deploy-app/resources/supervisor.conf /etc/supervisor/conf.d/microblog.conf
+sudo cp scripts/deploy-app/resources/supervisor.conf /etc/supervisor/conf.d/microblog-1.conf
 
 sudo mkdir /var/log/microblog
 sudo chmod 755 /var/log/microblog
