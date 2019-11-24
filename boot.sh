5d38075a3d82c1d3d463c82b3b523ebd24970e27
Unsinkablesam <maraphostv@gmail.com>
2019-11-25 00:23:41 +0100

    updated tasks #2

--- boot.sh
+++ boot.sh
@@ -10,4 +10,4 @@ while true; do
 done
 flask translate compile
 #exec gunicorn -b :5000 --access-logfile - --error-logfile - microblog:app
-exec gunicorn --statsd-host=localhost:9125 --statsd-prefix=helloworld -b :5000 --access-logfile - --error-logfile - microblog:app
+exec gunicorn --statsd-host=localhost:9125 --statsd-prefix=helloworld --bind 127.0.0.1::5000 microblog:app
