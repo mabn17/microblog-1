4bc91ba84651c2daa71c119200ba2d6d52122c8b
Unsinkablesam <maraphostv@gmail.com>
2019-11-30 23:30:31 +0100

    Now more SECURE, kmom05

--- boot.sh
+++ boot.sh
@@ -11,4 +11,4 @@ done
 flask translate compile
 #exec gunicorn -b :5000 --access-logfile - --error-logfile - microblog:app
 exec gunicorn --statsd-host=localhost:9125 --statsd-prefix=helloworld --bind :5000 --access-logfile - --error-logfile - microblog:app
-# gunicorn --statsd-host=localhost:9125 --statsd-prefix=helloworld --bind :5000 hello:app
+# gunicorn --statsd-host=localhost:9125 --statsd-prefix=helloworld --bind :5000 hello:ap
